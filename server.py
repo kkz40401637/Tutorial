8a4866ddccde760962d71cd6317ae10632e9be67
kkz40401637 <kaungkhantzin123@gmail.com>
2018-05-02 11:23:26 +0630

    JQuery

--- server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app=Flask(__name__)
-
-@app.route('/')
-def root():
-	return render_template('index.html'), 200
-
-if __name__ == "__main__":
-        app.run(debug=True)
